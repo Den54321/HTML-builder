611bda7ce710d24821daf1a93fc53871b17f60cb
Den54321 <dennbuika@gmail.com>
2023-05-08 16:54:26 +0300

    feat: add solution for 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const fspromises = require('fs/promises');
+const pach = require('path');
+const pachFolder = pach.join(__dirname, 'files');
+
+const promise  = fspromises.mkdir(pach.join( __dirname, 'files-copy'), { recursive: true });
+
+promise.then(
+    (res) =>{
+    if(!res){
+        fs.readdir(pach.join(__dirname, 'files-copy'), (err, res)=>{
+            res.forEach( item =>{
+                fs.unlink(pach.join(__dirname, 'files-copy', item), (err)=>{})   
+            })   
+          })  
+
+    }
+
+       fs.readdir(pachFolder, (err, res)=>{
+         res.forEach( item =>{
+
+          fs.copyFile(pach.join(pachFolder, item), pach.join(__dirname, 'files-copy', item),()=>{});
+
+         })
+         
+       }) 
+        
+    },
+    err=>{
+
+    }
+)
 
